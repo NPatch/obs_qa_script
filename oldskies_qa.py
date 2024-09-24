cf97091673a839d3639f6a3d27dcc63947fd0fcb
STARLORD\npats <npatsiouras@gmail.com>
2024-09-24 11:25:07 +0300

    Added new properties UI for all variables the user is expected to provide info for
    * Storage and retrieval of settings is done by obs
    * We only maintain our own runtime version of the data the user inputs
    * Cleanup

--- oldskies_qa.py
+++ oldskies_qa.py
@@ -28,35 +28,49 @@ last_app_status = None
 # Description displayed in the Scripts dialog window
 def script_description():
   return """<center><h2>Old Skies QA Tools</h2></center>
-
-# def script_defaults(settings):
-#     print("script_defaults")
             <p>Tools that automate and help in QA testing AGS games</p>"""
+
+def script_defaults(settings):
+    obs.script_log(obs.LOG_INFO, "script_defaults")
+    obs.obs_data_set_default_string(settings, "scene_name", "")
+    obs.obs_data_set_default_string(settings, "source_name", "")
+    obs.obs_data_set_default_int(settings, "steam_gameid", 0)
+    obs.obs_data_set_default_string(settings, "exe_name", "")
+    obs.obs_data_set_default_string(settings, "win_name", "")
+    obs.obs_data_set_default_string(settings, "win_class", "")
+    obs.obs_data_set_default_string(settings, "crash_win_name", "")
+    obs.obs_data_set_default_string(settings, "crash_win_name", "")
 
 def script_load(settings):
     obs.script_log(obs.LOG_INFO, "script_load")
     obs.obs_frontend_add_event_callback(on_frontend_finished_loading)
 
-# def script_update(settings):
-#     print("script_update")
-
 def script_properties():
     obs.script_log(obs.LOG_INFO, "script_properties")
     props = obs.obs_properties_create()
 
+    obs.obs_properties_add_text(props, "scene_name", "Scene name", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "source_name", "Source name", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "steam_gameid", "Steam GameID", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "exe_name", "Executable Name", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "win_name", "Window Name", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "win_class", "Window Class", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "crash_win_name", "Crash Window Name", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "crash_win_class", "Crash Window Class", obs.OBS_TEXT_DEFAULT)
+
     obs.obs_properties_add_button(props, "button0", "Start QA",start_qa)
-    obs.obs_properties_add_button(props, "button1", "Print Scene Info",print_scene_info)
-    obs.obs_properties_add_button(props, "button2", "Enum Scene Items",enum_scene_items)
-    obs.obs_properties_add_button(props, "button3", "Create Source",create_game_capture_source)
-    #obs.obs_properties_add_button(props, "button1", "Start QA:",start_qa)
     return props
 
-
-# def print_scene_info(props, property):
-#     obsutil.print_scene_info(ags_data.scene_name)
-
-# def enum_scene_items(props, property):
-#    obsutil.enum_scene_items(ags_data.scene_name)
+def script_update(settings):
+    obs.script_log(obs.LOG_INFO, "script_update")
+    ags_data.scene_name = obs.obs_data_get_string(settings, "scene_name")
+    ags_data.source_name = obs.obs_data_get_string(settings, "source_name")
+    ags_data.steam_gameid = obs.obs_data_get_string(settings, "steam_gameid")
+    ags_data.exe_name = obs.obs_data_get_string(settings, "exe_name")
+    ags_data.window_name = obs.obs_data_get_string(settings, "win_name")
+    ags_data.window_class = obs.obs_data_get_string(settings, "win_class")
+    ags_data.crash_window_name = obs.obs_data_get_string(settings, "crash_win_name")
+    ags_data.crash_window_class = obs.obs_data_get_string(settings, "crash_win_class")
 
 def create_game_capture_source(props, property):
     obs.script_log(obs.LOG_INFO, "create_game_capture_source")
@@ -179,6 +193,6 @@ def on_frontend_finished_loading(event):
     if event == obs.OBS_FRONTEND_EVENT_FINISHED_LOADING:
         setup_needs()
 
-#def script_unload():
-#   obs.script_log(obs.LOG_INFO, "script_unload")
-#    unset_signals()
+def script_unload():
+    obs.script_log(obs.LOG_INFO, "script_unload")
+    #unset_signals()
